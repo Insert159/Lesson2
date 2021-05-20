cddb1a6a75732cafc54e9936f7d23bf972abbf55
geekbrains <info@geekbrains.ru>
2021-05-20 19:03:02 +0700

    Initial commit

---
+++ src/main/java/ru/geekbrains/lesson2/MainApp.java
@@ -0,0 +1,60 @@
+package ru.geekbrains.lesson2;
+
+public class MainApp {
+
+
+    public static void main(String[] args) {
+        /*isSumBetween10And20();
+        isPositiveOrNegative();
+        isNegative();
+        printWordNTimes();*/
+        methodWithAStar();
+    }
+
+    public static boolean isSumBetween10And20() {
+        int a =5;
+        int b= 1;
+        int sum = a+b;
+        if (sum>=10 && sum<=20) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void isPositiveOrNegative() {
+        int a=10;
+        if (a>0) {
+            System.out.println("Число положительное");
+        } else {
+            System.out.println("Число отрицательное");
+        }
+    }
+
+    public static boolean isNegative() {
+        int a = 10;
+        if (a<0){
+            return true;
+        } else
+        return false;
+    }
+
+    public static void printWordNTimes() {
+        for (int  a = 0; a < 10; a++) {
+            System.out.println("word");
+        }
+        System.out.println();
+    }
+
+    public static boolean methodWithAStar() {
+        int a = 25;
+        if ((a % 4 == 0) || (a % 400 == 0)) {
+            return true;
+        } if (a % 100 == 0) {
+            return false;
+        }else {
+            return false;
+        }
+    }
+
+}
